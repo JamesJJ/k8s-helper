d75a121d987e10174d4d409ca6fda91a12b0e46c
JamesJJ <jj@fcg.fyi>
2019-10-21 12:36:07 +0800

    k8s-helper.sh

---
+++ sh/k8s-helper.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# This depends on curl
+curl --version &> /dev/null
+if [ $? -ne 0 ];then
+  echo '== Please install CURL!' 1>&2
+  exit 0
+fi
+
+  
+# Confirm ISTIO is ready before continuing.
+
+if [ "X${KUBERNETES_SERVICE_HOST}" != "X" ]; then
+  echo '= Waiting for ISTIO'
+  i=0; while [ $i -le 30 ]; do
+    i=$(( i + 1 ))
+    curl --max-time 1 -sSf "http://localhost:15020/healthz/ready" && break
+    sleep 1
+  done
+fi
+
